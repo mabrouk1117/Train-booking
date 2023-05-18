348714c35e61112227aed82255280269804ceb10
Habiba Ihab <101571848+habibaihab@users.noreply.github.com>
2023-05-18 20:01:13 +0300

    Update SignUp.java

--- SignUp.java
+++ SignUp.java
@@ -4,21 +4,12 @@ import java.util.Scanner;
 public class SignUp {
     private Connection connection;
 
-    public SignUp() {
-        String url = "jdbc:mysql://localhost:3306/my_schema";
-        String username = "root";
-        String password = "1234";
-        try {
-            connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public SignUp(Connection connection) {
+        this.connection = connection;
     }
 
-    public void insertUser() {
+    public void insertCustomer(Scanner scanner) {
         try {
-            Scanner scanner = new Scanner(System.in);
-
             System.out.println("Enter the ID:");
             int id = scanner.nextInt();
             scanner.nextLine();
@@ -28,19 +19,17 @@ public class SignUp {
 
             System.out.println("Enter the last name:");
             String lastName = scanner.nextLine();
+
             System.out.println("Enter the PhoneNumber :");
             String phoneNumber = scanner.nextLine();
 
             System.out.println("Enter the email:");
             String email = scanner.nextLine();
 
-
             System.out.println("Enter the password:");
             String passwordInput = scanner.nextLine();
-            System.out.println("Customer Or Admain :");
-            String userType = scanner.nextLine();
 
-            String sql = "INSERT INTO user (userID, firstName, lastName,phoneNumber, Email, password,userType) VALUES (?, ?, ?, ?, ?,?,?)";
+            String sql = "INSERT INTO Customer (CustomerID, firstName, lastName, phoneNumber, Email, password) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, id);
             statement.setString(2, firstName);
@@ -48,8 +37,6 @@ public class SignUp {
             statement.setString(4, phoneNumber);
             statement.setString(5, email);
             statement.setString(6, passwordInput);
-            statement.setString(7, userType);
-
 
             int rowsInserted = statement.executeUpdate();
             if (rowsInserted > 0) {
@@ -59,8 +46,48 @@ public class SignUp {
             }
 
             statement.close();
-            scanner.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void insertAdmin(Scanner scanner) {
+        try {
+            System.out.println("Enter the ID:");
+            int id = scanner.nextInt();
+            scanner.nextLine();
 
+            System.out.println("Enter the first name:");
+            String firstName = scanner.nextLine();
+
+            System.out.println("Enter the last name:");
+            String lastName = scanner.nextLine();
+
+            System.out.println("Enter the PhoneNumber :");
+            String phoneNumber = scanner.nextLine();
+
+            System.out.println("Enter the email:");
+            String email = scanner.nextLine();
+
+            System.out.println("Enter the password:");
+            String passwordInput = scanner.nextLine();
+
+            String sql = "INSERT INTO Admin (AdminID, firstName, lastName, phoneNumber, Email, password) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            statement.setString(2, firstName);
+            statement.setString(3, lastName);
+            statement.setString(4, phoneNumber);
+            statement.setString(5, email);
+            statement.setString(6, passwordInput);
+
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Data inserted successfully.");
+            } else {
+                System.out.println("Failed to insert data.");
+            }
+
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -75,5 +102,4 @@ public class SignUp {
             e.printStackTrace();
         }
     }
-
 }
